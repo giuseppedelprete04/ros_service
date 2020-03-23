4d588b30c09657a21601f2cd50b3096871bd129c
Giuseppe <giuseppedelprete04@gmail.com>
2020-03-23 17:48:38 +0000

    first commit

---
+++ src/service_server.cpp
@@ -0,0 +1,23 @@
+#include "ros/ros.h"
+#include "ros_service/service.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+bool service_callback (ros_service::service::Request &req, ros_service::service::Response &res) {
+	std::stringstream ss;
+	ss<<"Received Here";
+	res.out = ss.str();
+	ROS_INFO("From Client [%s], Server says [%s]", req.in.c_str(), 		res.out.c_str());
+	return true;
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "service_server");
+	ros::NodeHandle nh;
+	ros::ServiceServer service = nh.advertiseService("service", 		service_callback);
+	ROS_INFO("Ready to receive from client.");
+	ros::spin();
+	return 0;
+}
